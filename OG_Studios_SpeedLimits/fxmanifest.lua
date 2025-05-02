934032aa87588606abca0238f71159bd5eb640b4
OGDev Studios <jryan75707@icloud.com>
2025-05-01 20:48:48 -0500

    Add files via upload

---
+++ OG_Studios_SpeedLimits/fxmanifest.lua
@@ -0,0 +1,24 @@
+-------------------------------------------------------------
+-- Made by: TheOGDev / OGDev Studios
+-- Name: OG_Studios_SpeedLimits
+-- Desc: Speed limit signs
+-- 
+-- 04-30-2025
+-------------------------------------------------------------
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TheOGDev / OGDev Studios'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/**'
+}
+
+client_scripts {
+    'config.lua',
+    'client/main.lua',
+}
